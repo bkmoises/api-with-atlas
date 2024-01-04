40eb9a4013e89b38205bc7ae12f6b714e68dca38
bkmoises <bk_moises@hotmail.com>
2024-01-04 17:49:17 -0300

    inclusão do .env

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
 const personRoute = require("./routes/person");
 
-const PORT = 3000;
-const connectionString =
-  "mongodb+srv://root:root@cluster0.jerxi6e.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 3000;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+
+const connectionString = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.jerxi6e.mongodb.net/?retryWrites=true&w=majority`;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
