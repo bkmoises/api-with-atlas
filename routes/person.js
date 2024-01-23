c182ebcdaf9ad4d3755887ae2e13e4a47667469d
bkmoises <bk_moises@hotmail.com>
2024-01-23 16:08:58 -0300

    refatoração das rotas getAll e create person

--- routes/person.js
+++ routes/person.js
@@ -3,25 +3,27 @@ const Person = require("../models/person");
 
 router
   .route("/")
-  .get((_req, res) => {
-    return Person.find().then((result) => {
-      if (result.length) return res.status(200).json(result);
-      res.status(204).json();
-    });
+  .get(async (_req, res) => {
+    const result = await Person.find();
+    if (result.length) return res.status(200).json(result);
+    return res.status(204).json();
   })
-  .post((req, res) => {
+  .post(async (req, res) => {
     const { name, mail, age } = req.body;
     const person = { name, mail, age };
-    const requiredFields = ["name", "mail", "age"];
+    const requiredFields = Object.keys(person);
 
     for (const field of requiredFields) {
       if (!req.body[field] || !req.body[field].length)
         return res.status(400).json({ error: `field ${field} is required` });
     }
 
-    return Person.create(person).then((result) => {
-      res.status(201).json(result);
-    });
+    const emailVerify = await Person.findOne({ mail });
+    if (emailVerify)
+      return res.status(409).json({ message: "email already exist" });
+
+    const createdPerson = await Person.create(person);
+    return res.status(201).json(createdPerson);
   });
 
 router.route("/:id");
