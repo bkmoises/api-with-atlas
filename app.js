2babeaaedaa3aad38a0c6268d11759e79a106be5
bkmoises <bk_moises@hotmail.com>
2024-01-23 15:25:02 -0300

    configuração do servidor

---
+++ app.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const personRoute = require("./routes/person");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const connectionString = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.jerxi6e.mongodb.net/?retryWrites=true&w=majority`;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/person", personRoute);
+
+app.get("/", (_req, res) => {
+  res.status(200).json({ message: "Hello World!" });
+});
+
+mongoose
+  .connect(connectionString)
+  .then(() => {
+    console.log("Database conected!");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => {
+    console.log(err);
+  });
