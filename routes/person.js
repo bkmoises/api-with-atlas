5af924a5378f76693f4eaf18604bb1bc25eb08eb
bkmoises <bk_moises@hotmail.com>
2024-01-23 16:27:16 -0300

    inclusão da rota de update person

--- routes/person.js
+++ routes/person.js
@@ -26,14 +26,28 @@ router
     return res.status(201).json(createdPerson);
   });
 
-router.route("/:id").get(async (req, res) => {
-  const { id } = req.params;
-  try {
-    const result = await Person.findOne({ _id: id });
-    if (result) return res.status(200).json(result);
-  } catch (err) {
-    return res.status(204).json();
-  }
-});
+router
+  .route("/:id")
+  .get(async (req, res) => {
+    const { id } = req.params;
+    try {
+      const result = await Person.findOne({ _id: id });
+      if (result) return res.status(200).json(result);
+    } catch (err) {
+      return res.status(204).json();
+    }
+  })
+  .put(async (req, res) => {
+    const { id } = req.params;
+    const { name, mail, age } = req.body;
+    const person = { name, mail, age };
+    try {
+      const updatedPerson = await Person.updateOne({ _id: id }, person);
+      if (updatedPerson.matchedCount) return res.status(200).json();
+      return res.status(204).json();
+    } catch (err) {
+      return res.status(500).json();
+    }
+  });
 
 module.exports = router;
