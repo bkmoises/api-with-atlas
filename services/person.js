93c49463e9af0af6be253b2a7a4f1b84314fea07
bkmoises <bk_moises@hotmail.com>
2024-01-24 11:33:42 -0300

    refatoração do código de serviços

--- services/person.js
+++ services/person.js
@@ -7,35 +7,35 @@ const {
 } = require("../controllers/person");
 
 const getAll = async (_req, res) => {
-  const response = await getAllPersonController();
-  return res.status(response.statusCode).json(response.body);
+  const { statusCode, body } = await getAllPersonController();
+  return res.status(statusCode).json(body);
 };
 
 const createPerson = async (req, res) => {
   const { name, mail, age } = req.body;
   const person = { name, mail, age };
-  const response = await createPersonController(person);
-  return res.status(response.statusCode).json(response.body);
+  const { statusCode, body } = await createPersonController(person);
+  return res.status(statusCode).json(body);
 };
 
 const getPerson = async (req, res) => {
   const { id } = req.params;
-  const response = await getPersonController(id);
-  return res.status(response.statusCode).json(response.body);
+  const { statusCode, body } = await getPersonController(id);
+  return res.status(statusCode).json(body);
 };
 
 const updatePerson = async (req, res) => {
   const { id } = req.params;
   const { name, mail, age } = req.body;
   const person = { name, mail, age };
-  const response = await updatePersonController(id, person);
-  return res.status(response.statusCode).json();
+  const { statusCode } = await updatePersonController(id, person);
+  return res.status(statusCode).json();
 };
 
 const deletePerson = async (req, res) => {
   const { id } = req.params;
-  const response = await deletePersonController(id);
-  return res.status(response.statusCode).json();
+  const { statusCode } = await deletePersonController(id);
+  return res.status(statusCode).json();
 };
 
 module.exports = {
